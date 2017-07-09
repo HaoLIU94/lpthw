58458093b560c4e14bb2af18a4f9014011381c48
Unknown <liuhaoxdu@gmail.com>
2017-07-09 09:13:53 +0200

    Write a poem

---
+++ ex4.py
@@ -0,0 +1,9 @@
+adjective1 = raw_input("Tell me an adjective, and click enter. ")
+noun1 = raw_input("Tell me a noun (plural), and click enter. ")
+noun2 = raw_input("Tell me another noun, and click enter. ")
+adjective2 = raw_input("Tell me an another adjective, and click enter. ")
+
+print "Roses are " + adjective1
+print noun1 + " are blue"
+print noun2 + " is " + adjective2
+print "And so are you!"
